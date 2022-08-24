e5a83cae789d336189e662d25e9d4cd56f96ce29
Jesse Tomchak <jtomchak@gmail.com>
2022-08-24 10:23:45 -0700

    serverless update

--- netlify/functions/events/index.js
+++ netlify/functions/events/index.js
@@ -51,7 +51,7 @@ async function handler(event) {
 //    2. Also use `redirects: "netlify-toml-builders"` in your config file’s serverless bundler options:
 //       https://www.11ty.dev/docs/plugins/serverless/#bundler-options
 
-exports.handler = handler;
+// exports.handler = handler;
 
 const { builder } = require("@netlify/functions");
 exports.handler = builder(handler);
